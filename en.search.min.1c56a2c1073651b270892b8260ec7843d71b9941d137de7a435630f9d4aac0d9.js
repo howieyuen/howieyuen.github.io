5288b8c7652053d3f469d22b605d80c9747070a9
howieyuen <howieyuen@users.noreply.github.com>
2020-10-20 14:48:40 +0000

    add StatefulSet Controller 源码分析 2f9831802dc623e0e3970e5af5e480a766660c49

--- en.search.min.1c56a2c1073651b270892b8260ec7843d71b9941d137de7a435630f9d4aac0d9.js
+++ en.search.min.1c56a2c1073651b270892b8260ec7843d71b9941d137de7a435630f9d4aac0d9.js
@@ -3,7 +3,7 @@ input.addEventListener('focus',init);input.addEventListener('keyup',search);docu
 const characterPressed=String.fromCharCode(event.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();event.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.f8934042fff73acd4c9982021bf5612da5656d59e04a1b13c92b1daacd21376e.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.3ac678e3b86783299a393b845e1ca36f110e409cb64316105549212b3dee10ae.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=element('<li><a href></a><small></small></li>');const a=li.querySelector('a'),small=li.querySelector('small');a.href=page.href;a.textContent=page.title;small.textContent=page.section;results.appendChild(li);});}
